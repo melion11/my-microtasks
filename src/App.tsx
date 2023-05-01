88d4726a09d28edc004ab2de61466dde5420f81f
melion11 <gphooenix@gmail.com>
2023-05-01 14:59:38 +0300

    second microtask about button

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,42 @@ import React from 'react';
 import {Cars, carType} from './map/topCars';
 
 function App() {
-
     const topCars: carType[] = [
-        {manufacturer:'BMW', model:'m5cs'},
-        {manufacturer:'Mercedes', model:'e63s'},
-        {manufacturer:'Audi', model:'rs6'}
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
     ]
 
-  return (
-    <div className="App">
-      <Cars cars={topCars}/>
-    </div>
-  )
+    // function myFirstSubscriber () {
+    //     console.log('Hello, i am Vasya')
+    // }
+    //
+    // function mySecondSubscriber () {
+    //     console.log('Hello, i am Ivan')
+    // }
+
+    // const onClickHandler = (name: string) => {
+    //     console.log(name)
+    // }
+
+    const foo1 = () => {
+        console.log(100200)
+    }
+
+    const foo2 = (num:number) => {
+       console.log(`в эту функцию пришло число ${num}`)
+    }
+
+    return (
+        <div className="App">
+            <Cars cars={topCars}/>
+            {/*<button onClick={(e)=> onClickHandler('Ivan')}>MyYouTubeChanel-1</button>*/}
+            {/*<button onClick={(e)=> onClickHandler('Vasya')}>MyYouTubeChanel-2</button>*/}
+
+            <button onClick={foo1}>1</button>
+            <button onClick={()=> foo2(100200)}>2</button>
+        </div>
+    )
 }
 
 export default App;
